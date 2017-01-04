70fabd6b359dcfc13bf5137e6b2e800afc8f7276
Laurent-PANEK <laurent.panek@ynov.com>
2017-01-04 15:57:29 +0100

    ajout php

--- php/DB_LINK.php
+++ php/DB_LINK.php
@@ -1,8 +1,4 @@
 <?php
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 /* Connexion à une base ODBC avec l'invocation de pilote */
 $dsn = 'mysql:dbname=be-guided;host=127.0.0.1';
 $user = 'root';
@@ -13,29 +9,3 @@ try {
 } catch (PDOException $e) {
     echo 'Connexion échouée : ' . $e->getMessage();
 }
-=======
-=======
->>>>>>> 7112b3f7a1eb03627ffafe92ea93c60c5302125b
-=======
->>>>>>> 7112b3f7a1eb03627ffafe92ea93c60c5302125b
-=======
->>>>>>> 7112b3f7a1eb03627ffafe92ea93c60c5302125b
-/**
- * Created by PhpStorm.
- * User: Laurent
- * Date: 04/01/2017
- * Time: 13:41
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
- */
->>>>>>> 7112b3f7a1eb03627ffafe92ea93c60c5302125b
-=======
- */
->>>>>>> 7112b3f7a1eb03627ffafe92ea93c60c5302125b
-=======
- */
->>>>>>> 7112b3f7a1eb03627ffafe92ea93c60c5302125b
-=======
- */
->>>>>>> 7112b3f7a1eb03627ffafe92ea93c60c5302125b
