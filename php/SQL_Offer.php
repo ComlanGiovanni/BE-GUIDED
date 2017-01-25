159e2277db4794a0d53110af007f84b02b59fbde
Laurent-PANEK <laurent.panek@ynov.com>
2017-01-25 10:10:44 +0100

    update SQL_offer

--- php/SQL_Offer.php
+++ php/SQL_Offer.php
@@ -32,4 +32,23 @@ class SQL_Offer
         return $req;
     }
 
+    public function view_offer()
+    {
+        $v = $this->query('SELECT * FROM `offers` WHERE `id_offer` = :id',
+            [':id' => $_GET['id']])->fetchAll();
+        return $v;
+    }
+
+    public function list_offer()
+    {
+        $i = $this->query('SELECT id_offer,name_offer,price,description,city,img_offer,id_user FROM offers ORDER BY id_offer DESC')->fetchAll();
+        foreach ($i as $img) {
+            echo '<tr>
+                <td><img src="' . $img['img_offer'] . '" height="200px" width="200px" alt=""></td>
+                <td>' . $img['city'] . '</td>
+                <td><h4>' . $img['name_offer'] .'</h4><br><br><p>' . $img['description'] .'</p></td>
+                <td> ' . $img['price'] . ' €</td>
+                </tr>';
+        }
+    }
 }
